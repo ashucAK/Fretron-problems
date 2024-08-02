144c603ff33e17e047fb21734c11700be524a850
ashucAK <singhashishk12@gmail.com>
2024-08-02 16:25:36 +0530

    second question

--- Problem2/AppleDistribution.java
+++ Problem2/AppleDistribution.java
@@ -1,67 +1,67 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AppleDistribution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> apples = new ArrayList<>();
 
-        // Input apple weights
-        while (true) {
-            System.out.print("Enter apple weight in grams (-1 to stop): ");
-            int weight = scanner.nextInt();
-            if (weight == -1) break;
-            apples.add(weight);
+    // Distribution of apples among shares
+    private static void distributeApple(List<List<Integer>> appleQuantity, List<Integer> appleWeight, List<Integer> shares, List<Integer> sharesWeight) {
+        for (int i = 0; i < appleWeight.size(); i++) {
+            int minIndex = 0;
+            for (int j = 1; j < shares.size(); j++) {
+                if (sharesWeight.get(j) < sharesWeight.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            sharesWeight.set(minIndex, sharesWeight.get(minIndex) + appleWeight.get(i));
+            appleQuantity.get(minIndex).add(appleWeight.get(i));
         }
 
-        // Sort apples in descending order
-        Collections.sort(apples, Collections.reverseOrder());
-
-        // Define shares
-        int total = 100;
-        int[] shares = {50, 30, 20};
-        String[] names = {"Ram", "Sham", "Rahim"};
-
-        // Distribute apples
-        List<List<Integer>> distribution = distributeApples(apples, shares);
-
-        // Print results
-        System.out.println("Distribution Result:");
-        for (int i = 0; i < names.length; i++) {
-            System.out.print(names[i] + ": ");
-            System.out.println(distribution.get(i).toString().replaceAll("[\\[\\]]", ""));
+        for (int i = 0; i < shares.size(); i++) {
+            System.out.print("Share " + (i + 1) + " : ");
+            for (int j = 0; j < appleQuantity.get(i).size(); j++) {
+                System.out.print(appleQuantity.get(i).get(j) + " ");
+            }
+            System.out.println();
         }
     }
 
-    public static List<List<Integer>> distributeApples(List<Integer> apples, int[] shares) {
-        List<List<Integer>> distribution = new ArrayList<>();
-        for (int i = 0; i < shares.length; i++) {
-            distribution.add(new ArrayList<>());
-        }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> appleWeight = new ArrayList<>();
 
-        int totalWeight = apples.stream().mapToInt(Integer::intValue).sum();
-        int[] targetWeights = new int[shares.length];
-        for (int i = 0; i < shares.length; i++) {
-            targetWeights[i] = totalWeight * shares[i] / 100;
-        }
-
-        int[] currentWeights = new int[shares.length];
-
-        for (int apple : apples) {
-            int bestIndex = 0;
-            double bestRatio = Double.MAX_VALUE;
-
-            for (int i = 0; i < shares.length; i++) {
-                double ratio = (double) (currentWeights[i] + apple) / targetWeights[i];
-                if (ratio < bestRatio) {
-                    bestRatio = ratio;
-                    bestIndex = i;
-                }
+        // Taking input of apple weight
+        System.out.println("Enter the weight of apples (-1 to stop): ");
+        while (true) {
+            int weight = scanner.nextInt();
+            if (weight == -1) {
+                break;
             }
-
-            distribution.get(bestIndex).add(apple);
-            currentWeights[bestIndex] += apple;
+            appleWeight.add(weight);
         }
 
-        return distribution;
+        // Array of shares
+        List<Integer> shares = List.of(50, 30, 20);
+        List<Integer> sharesWeight = new ArrayList<>(List.of(0, 0, 0));
+
+        List<List<Integer>> appleQuantity = new ArrayList<>();
+        for (int i = 0; i < shares.size(); i++) {
+            appleQuantity.add(new ArrayList<>());
+        }
+
+        distributeApple(appleQuantity, appleWeight, shares, sharesWeight);
+
+        List<String> names = List.of("Ram", "Sham", "Rahim");
+
+        // Printing the distribution of apples among shares
+        for (int i = 0; i < shares.size(); i++) {
+            System.out.print(names.get(i) + " : ");
+            for (int j = 0; j < appleQuantity.get(i).size(); j++) {
+                System.out.print(appleQuantity.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
     }
 }
